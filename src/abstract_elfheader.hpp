4eb98f0c04ee1a675d7aa58802855c3ac526b62e
VitorMob <mobhacking100@gmail.com>
2021-12-13 15:28:18 -0300

    directives compiler edited

--- src/abstract_elfheader.hpp
+++ src/abstract_elfheader.hpp
@@ -15,12 +15,12 @@
 #include <cstring>
 #include <stdexcept>
 
-#define CEXIT_SUCCESS " ";
-
-#if WINDOWS
+#ifdef __APPLE__
+    #include "endian.h"
+#elif WINDOWS
     #include <WinSock2.h>
     #include "endian.h"
-#else 
+#else
     #include <arpa/inet.h>
 #endif
 
@@ -36,111 +36,111 @@ namespace elf
  */
 class AbstractElfHeader
 {
-    private:
-        // disable evil things
-        AbstractElfHeader(const AbstractElfHeader &p_rhs);
-        AbstractElfHeader &operator=(const AbstractElfHeader &p_rhs);
+private:
+    // disable evil things
+    AbstractElfHeader(const AbstractElfHeader &p_rhs);
+    AbstractElfHeader &operator=(const AbstractElfHeader &p_rhs);
 
-        // indicates if the binary is 64 bit or not
-        bool m_is64;
+    // indicates if the binary is 64 bit or not
+    bool m_is64;
 
-        // stores the file size due to true.asm silliness
-        boost::uint32_t m_fileSize;
+    // stores the file size due to true.asm silliness
+    boost::uint32_t m_fileSize;
 
-        // a pointer to the 32 bit version of the ELF header
-        const elf::elf_header_32 *m_header32;
+    // a pointer to the 32 bit version of the ELF header
+    const elf::elf_header_32 *m_header32;
 
-        // a pointer to the 64 bit version of the ELF header
-        const elf::elf_header_64 *m_header64;
+    // a pointer to the 64 bit version of the ELF header
+    const elf::elf_header_64 *m_header64;
 
-    public:
-        // default initialize members
-        AbstractElfHeader();
-        ~AbstractElfHeader();
+public:
+    // default initialize members
+    AbstractElfHeader();
+    ~AbstractElfHeader();
 
-        //p_data a pointer to the elf header
-        //p_size the total size of p_data
-        void setHeader(const char *p_data, std::size_t p_size);
+    // p_data a pointer to the elf header
+    // p_size the total size of p_data
+    void setHeader(const char *p_data, std::size_t p_size);
 
-        // return true if and only if the binary is 64 bit
-        bool is64() const;
+    // return true if and only if the binary is 64 bit
+    bool is64() const;
 
-        // return the four magic bytes
-        std::string getMagic() const;
+    // return the four magic bytes
+    std::string getMagic() const;
 
-        // return the class of the file (32 vs. 64 bit)
-        std::string getClass() const;
+    // return the class of the file (32 vs. 64 bit)
+    std::string getClass() const;
 
-        // return the encoding (le vs be)
-        std::string getEncoding() const;
+    // return the encoding (le vs be)
+    std::string getEncoding() const;
 
-        // return the ELF file version
-        std::string getFileVersion() const;
+    // return the ELF file version
+    std::string getFileVersion() const;
 
-        // return the OS ABI
-        std::string getOSABI() const;
+    // return the OS ABI
+    std::string getOSABI() const;
 
-        // return the ABI Version
-        std::string getABIVersion() const;
+    // return the ABI Version
+    std::string getABIVersion() const;
 
-        // return the binary type (exec, relocatable, etc)
-        std::string getType() const;
+    // return the binary type (exec, relocatable, etc)
+    std::string getType() const;
 
-        // return the flags
-        std::string getFlags() const;
+    // return the flags
+    std::string getFlags() const;
 
-        // return the elf header size
-        boost::uint16_t  getEHSize() const;
+    // return the elf header size
+    boost::uint16_t getEHSize() const;
 
-        // return the entry point
-        std::string getEntryPointString() const;
+    // return the entry point
+    std::string getEntryPointString() const;
 
-        // return the elf version part 2
-        std::string getVersion() const;
+    // return the elf version part 2
+    std::string getVersion() const;
 
-        // return the virtual address of the entry pointer
-        boost::uint64_t getEntryPoint() const;
+    // return the virtual address of the entry pointer
+    boost::uint64_t getEntryPoint() const;
 
-        // return the offset to the program header
-        boost::uint32_t getProgramOffset() const;
+    // return the offset to the program header
+    boost::uint32_t getProgramOffset() const;
 
-        // return the number of expected program header entries
-        boost::uint16_t getProgramCount() const;
+    // return the number of expected program header entries
+    boost::uint16_t getProgramCount() const;
 
-        // return the size of a program header entry
-        boost::uint16_t getProgramSize() const;
+    // return the size of a program header entry
+    boost::uint16_t getProgramSize() const;
 
-        // return the offset to the section header
-        boost::uint32_t getSectionOffset() const;
+    // return the offset to the section header
+    boost::uint32_t getSectionOffset() const;
 
-        // return the size of a section table entry
-        boost::uint16_t getSectionSize() const;
+    // return the size of a section table entry
+    boost::uint16_t getSectionSize() const;
 
-        // return the number of entries in the section header
-        boost::uint16_t getSectionCount() const;
+    // return the number of entries in the section header
+    boost::uint16_t getSectionCount() const;
 
-        // return the index of the section header string table
-        boost::uint16_t getStringTableIndex() const;
+    // return the index of the section header string table
+    boost::uint16_t getStringTableIndex() const;
 
-        // return the offset of the section header string table
-        boost::uint32_t getStringTableOffset(const char *p_start) const;
+    // return the offset of the section header string table
+    boost::uint32_t getStringTableOffset(const char *p_start) const;
 
-        // return an if there is le or not
-        bool isLE() const;
+    // return an if there is le or not
+    bool isLE() const;
 
-        // return the architecture this file is made for
-        std::string getMachine() const;
+    // return the architecture this file is made for
+    std::string getMachine() const;
 
-        // return a string representation of the elf header
-        std::string printToStdOut() const;
+    // return a string representation of the elf header
+    std::string printToStdOut() const;
 
-        /* 
-        examines the elf header results and stores information into one of the two containers.
-        p_reasons the reasons for scoring vector
-        p_capabilities the capabilities map
-        */
-        void evaluate(std::vector<std::pair<boost::int32_t, std::string>> &p_reasons, 
-            std::map<elf::Capabilties, std::set<std::string>> &p_capabilities) const;
+    /*
+    examines the elf header results and stores information into one of the two containers.
+    p_reasons the reasons for scoring vector
+    p_capabilities the capabilities map
+    */
+    void evaluate(std::vector<std::pair<boost::int32_t, std::string>> &p_reasons,
+                  std::map<elf::Capabilties, std::set<std::string>> &p_capabilities) const;
 };
 
 #endif
