cf46e3a9791cea6204399062286676f30e576a0a
VitorMob <mobhacking100@gmail.com>
2021-12-28 09:05:53 -0300

    edited extension name

--- lib/hash-lib/sha256.cpp
+++ lib/hash-lib/sha256.cpp
@@ -4,11 +4,11 @@
 // see http://create.stephan-brumme.com/disclaimer.html
 //
 
-#include "sha256.h"
+#include "sha256.hpp"
 
 // big endian architectures need #define __BYTE_ORDER __BIG_ENDIAN
 #ifndef _MSC_VER
-#include "../../src/endian.h"
+#include "../../src/endian.hpp"
 #endif
 
 
