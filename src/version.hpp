bbbc3f788567ce6d69ed0ea148e4bffb4732111c
VitorMob <mobhacking100@gmail.com>
2022-02-03 14:30:58 -0300

    [UPD] copyright edited

--- src/version.hpp
+++ src/version.hpp
@@ -1,11 +1,9 @@
 #ifndef SC_VERSION_HPP
 #define SC_VERSION_HPP
 
-#include <string>
-
 std::string version_elf_parser()
 {
-    return std::string("Version: ELF Parser 1.4.0\nCopyright:\n  elfparser-ng (c) 2021 Mente Binária\n  elfparser  (c) 2014-2021 Jacob Baines");
+    return std::string("Version: ELF Parser ng 1.5.0 \n Copyright: elfparser-ng (c) 2021 Mente Binária\n  elfparser  (c) 2014-2021 Jacob Baines");
 }
 
 #endif // !SC_VERSION_HPP
